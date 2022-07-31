d2b53ecc3f04cdc1448829cf0a7f03c1f1954fe5
Samsara9527 <cnhwvista@gmail.com>
2022-07-31 11:39:42 +0800

    jump to xiaoetong miniprogram from home page

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,27 @@
+import React from "react";
+
+import { Image, View } from "@tarojs/components";
+import { navigateTo, navigateToMiniProgram } from "@tarojs/taro";
+import { Arrow } from "@taroify/icons";
+
 import ListItem from "@/comps/ListItem";
 import TabBar from "@/comps/TabBar";
+
 import Train from "@/static/icons/jujia.svg";
 import SelfTest from "@/static/icons/self-test.svg";
-import { Arrow } from "@taroify/icons";
-import { Image, View } from "@tarojs/components";
-import { navigateTo } from "@tarojs/taro";
-import React from "react";
+import VideoCourse from "@/static/icons/video.svg";
+
 import "./index.scss";
 
 export default function App() {
   const goto = url => {
     navigateTo({ url });
+  };
+
+  const gotoOther = () => {
+    navigateToMiniProgram({
+      appId: 'wx98dc9b974915de77',
+    });
   };
 
   return (
@@ -39,6 +50,22 @@ export default function App() {
             left={
               <View className="left">
                 <Image src={Train} className="icon" />
+              </View>
+            }
+            right={
+              <View className="arrow-icon">
+                <Arrow color="#fff" />
+              </View>
+            }
+          />
+        </View>
+        <View className="list" onClick={() => gotoOther()}>
+          <ListItem
+            title="视频课程"
+            subTitle="蕾波视频，助力康复"
+            left={
+              <View className="left">
+                <Image src={VideoCourse} className="icon" />
               </View>
             }
             right={
