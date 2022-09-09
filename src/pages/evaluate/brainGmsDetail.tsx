4dced3a9e90c62a177fa4480491e068eca6921b0
jiwenjiang <513755565@qq.com>
2022-09-09 21:58:13 +0800

    fix:bug

--- src/pages/evaluate/brainGmsDetail.tsx
+++ src/pages/evaluate/brainGmsDetail.tsx
@@ -4,6 +4,7 @@ import Report from "@/comps/Report.tsx";
 import { useReportBtnHandle } from "@/service/hook";
 import request from "@/service/request";
 import fenxiImg from "@/static/imgs/fenxi.png";
+import introImg from "@/static/imgs/intro.png";
 import pingceImg from "@/static/imgs/pingce.png";
 import yonghuImg from "@/static/imgs/yonghu.jpg";
 import { Button, Dialog, Popup } from "@taroify/core";
@@ -11,6 +12,7 @@ import { InfoOutlined } from "@taroify/icons";
 import { Image, Text, View } from "@tarojs/components";
 import { useRouter } from "@tarojs/taro";
 import React, { useEffect, useState } from "react";
+import { cls } from "reactutils";
 import styles from "./brainDetail.module.scss";
 
 const colorMap = {
@@ -40,34 +42,42 @@ const riskMap = {
 
 const intros = [
   {
-    title: "1、单调性（PR）：",
+    title: "1、正常扭动运动（N）：【扭动阶段】",
+    content:
+      "出现在足月至足月后6~9周龄内。其特征为小至中等幅度，速度缓慢至中等，运动轨迹在形式上呈现为椭圆体，给人留下扭动的印象。"
+  },
+  {
+    title: "2、正常不安运动（F+）：【不安阶段】",
+    content:
+      "是一种小幅度中速运动，遍布颈、躯干和四肢,发生在各个方向，运动加速度可变，在清醒婴儿中该运动持续存在(哭闹时除外)，通常在足月后9周龄左右出现。早产儿可在矫正年龄足月后6周龄左右出现不安运动。不安运动出现的频度随年龄而发生改变，一般可以分为：①连续性不安运动：指不安运动时常出现,间以短时间暂停。不安运动发生在整个身体，尤其在颈、躯干、肩、腕、髋和踝部。不安运动在不同身体部位的表现可能不同，取决于身体姿势尤其是头部位置。②间歇性不安运动：指不安运动之间的暂停时间延长，令人感觉到不安运动在整个观察时期内仅出现一半。③偶发性不安运动：不安运动之间的暂停时间更长。"
+  }
+];
+
+const intros2 = [
+  {
+    title: "1、单调性（PR）：【扭动阶段】",
     content:
       "表现为宝宝连续性运动顺序的单调，不同身体部位的运动失去了正常的GMS复杂性，总是简单的重复几个动作。存在一定的神经运动发育障碍风险。"
   },
   {
-    title: "2、痉挛－同步性（CS）：",
+    title: "2、痉挛－同步性（CS）：【扭动阶段】",
     content:
       "扭动运动阶段出现运动僵硬，失去正常的流畅性，所有肢体和躯干肌肉几乎同时收缩和放松，比如双腿同时抬高并且同时放下。存在神经运动发育障碍风险。"
   },
   {
-    title: "3、混乱型（CH）：",
+    title: "3、混乱型（CH）：【扭动阶段】",
     content:
       "扭动运动阶段出现肢体运动幅度大，顺序混乱，失去流畅性，动作突然不连贯。“混乱型”相当少见，常在数周后发展为“痉挛－同步性”GMs。存在神经运动发育障碍风险。"
   },
   {
-    title: "4、不安运动缺乏（F-）：",
+    title: "4、不安运动缺乏（F-）：【不安阶段】",
     content:
       "不安运动是一种小幅度，中速度的细微运动，在9-20周龄的宝宝身上会如星辰般闪烁的各个的身体部位上。如果没有这样的细微运动出现，便是不安运动缺乏了。存在神经运动发育障碍风险。"
   },
   {
-    title: "5、扭动运动正常（N）",
+    title: "5、异常不安运动 （AF）：【不安阶段】",
     content:
-      "自发运动符合年龄水平。出现在足月6-9周龄内，其特征为小至中等幅度，速度缓慢至中等，运动轨迹在形式上呈现为椭圆形。发展为明显的神经运动障碍可能性很小。"
-  },
-  {
-    title: "6、自发性全身运动正常(F+)：",
-    content:
-      "不安运动正常出现，符合年龄水平。不安运动是一种小幅度，中速度的细微运动，在9-20周龄的宝宝身上会如星辰般闪烁的各个的身体部位上。发展为明显的神经运动障碍可能性很小。"
+      "看起来与正常不安运动相似，但在动作幅度、速度以及不平稳性方面中度或明显夸大。该异常模式少见, 并且预测价值低。"
   }
 ];
 
@@ -184,33 +194,8 @@ function Card() {
               <View className={styles.cardBox}>
                 <View className={styles.card}>
                   <View className={styles.title}>
-                    <Image src={pingceImg} className={styles.imgIcon} />
-                    &nbsp; GMs测评结果{" "}
-                    <InfoOutlined
-                      size={17}
-                      style={{ marginLeft: 5 }}
-                      color="#1989fa"
-                      onClick={() => setIntro(true)}
-                    />
-                  </View>
-                  <View className={styles.gmsEvaBox}>
-                    {report.scaleResult?.gmsResult?.result?.map(
-                      (v, i) =>
-                        v.content && (
-                          <View key={i} className={styles.evaItem}>
-                            <View className={styles.evaTitle}>{v.name}</View>
-                            <View className={styles.evaVal}>{v.content}</View>
-                          </View>
-                        )
-                    )}
-                  </View>
-                </View>
-              </View>
-              <View className={styles.cardBox}>
-                <View className={styles.card}>
-                  <View className={styles.title}>
                     <Image src={fenxiImg} className={styles.imgIcon} />
-                    &nbsp; 结果分析
+                    &nbsp; 蕾波自测结果分析
                   </View>
                   <View className={styles.remark}>
                     <View>
@@ -242,21 +227,23 @@ function Card() {
                     </Text>
                   </View>
                   <View className={styles.evaBox}>
-                    <View
-                      className={styles.tag}
-                      style={{
-                        backgroundColor:
+                    {report?.scaleResult?.cerebralPalsyResult?.result && (
+                      <View
+                        className={styles.tag}
+                        style={{
+                          backgroundColor:
+                            checkItem(
+                              report?.scaleResult?.cerebralPalsyResult?.result
+                            )?.color ?? "#000"
+                        }}
+                      >
+                        {
                           checkItem(
                             report?.scaleResult?.cerebralPalsyResult?.result
-                          )?.color ?? "#000"
-                      }}
-                    >
-                      {
-                        checkItem(
-                          report?.scaleResult?.cerebralPalsyResult?.result
-                        )?.text
-                      }
-                    </View>
+                          )?.text
+                        }
+                      </View>
+                    )}
 
                     <View className={styles.tagBox}>
                       {report.scaleResult?.cerebralPalsyResult?.highRisk?.map(
@@ -276,20 +263,52 @@ function Card() {
                         )
                       )}
                     </View>
+                    <View className={styles.mt20}>
+                      <View className={styles.intro}>
+                        评估结果不代表诊断结果
+                      </View>
+                    </View>
                   </View>
                 </View>
               </View>
+              {/* <View className={styles.cardBox}>
+                <View className={styles.card}>
+                  <View className={styles.title}>
+                    <Image src={pingceImg} className={styles.imgIcon} />
+                    &nbsp; GMs测评结果
+                  </View>
+                </View>
+              </View> */}
               <View className={styles.cardBox}>
                 <View className={styles.card}>
                   <View className={styles.title}>
                     <Image src={fenxiImg} className={styles.imgIcon} />
-                    &nbsp; GMs医学评估
+                    &nbsp; GMs医学评估{" "}
+                    <InfoOutlined
+                      size={17}
+                      style={{ marginLeft: 5 }}
+                      color="#1989fa"
+                      onClick={() => setIntro(true)}
+                    />
                     <Text className={styles.evaDate}>
                       {report.evaluateDate}
                     </Text>
                   </View>
-                  <View className={styles.evaBox}>
-                    <View>{report.scaleResult?.gmsResult?.remark}</View>
+                  <View className={styles.gmsEvaBox}>
+                    {report.scaleResult?.gmsResult?.result?.map(
+                      (v, i) =>
+                        v.content && (
+                          <View key={i} className={styles.evaItem}>
+                            <View className={styles.evaTitle}>{v.name}</View>
+                            <View className={styles.evaVal}>{v.content}</View>
+                          </View>
+                        )
+                    )}
+                  </View>
+                  <View className={cls(styles.evaBox, styles.evaBox2)}>
+                    <View className={styles.evaRemark}>
+                      {report.scaleResult?.gmsResult?.remark}
+                    </View>
                     <View className={styles.intro}>评估结果不代表诊断结果</View>
                   </View>
                 </View>
@@ -335,7 +354,27 @@ function Card() {
         onClose={() => setIntro(false)}
         open={intro}
       >
+        <View>
+          <Image src={introImg} className={styles.introImg} />
+        </View>
+        <View className={styles.introBox}>
+          全身运动（GMs）是最常出现和最复杂的一种自发性运动模式，最早出现于妊娠9周的胎儿，持续至出生后5~6个月，能够十分有效地评估年幼神经系统的功能。GMs指整个身体参与的运动，臂、腿、颈和躯干以变化运动顺序的方式参与，这种GMs在运动强度、力量和速度方面具有高低起伏的变化，运动的开始和结束都具有渐进性。沿四肢轴线的旋转和运动方向的轻微改变使整个运动流畅优美并产生一种复杂多变的印象。
+        </View>
+        <View className={styles.introBox}>
+          GMs按时间的发育历程包括：足月前GMs（foetal and preterm
+          GMs，即胎儿和早产儿阶段），扭动运动（writhing
+          movements，WMs，即从足月至足月后6~9周龄）和不安运动（fidgety
+          movements，FMs，即足月后6~9周龄至5~6月龄）。
+        </View>
+        <View className={styles.introBox}>其中正常GMs主要有：</View>
         {intros.map(v => (
+          <View className={styles.introBox} key={v.title}>
+            <View className={styles.introTitle}>{v.title}</View>
+            <View>{v.content}</View>
+          </View>
+        ))}
+        <View className={styles.introBox}>其中异常的GMs主要包括：</View>
+        {intros2.map(v => (
           <View className={styles.introBox} key={v.title}>
             <View className={styles.introTitle}>{v.title}</View>
             <View>{v.content}</View>
