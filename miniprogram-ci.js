91b8a071d046b9862f91af3f8ea7c59015465d56
jiwenjiang <513755565@qq.com>
2022-12-29 16:32:14 +0800

    feat:test ci

--- miniprogram-ci.js
+++ miniprogram-ci.js
@@ -1,12 +1,37 @@
 (async () => {
   const execa = await import("execa");
+  const inquirer = await import("inquirer");
   const ci = require("miniprogram-ci");
   const projectJson = require("./project.config.json");
   const version = require("./version");
 
+  let comment = getGitLastMsg("%an/%cd/%s");
+
+  //   const answer = await inquirer.default.prompt([
+  //     {
+  //       type: "string",
+  //       name: "comment",
+  //       message: `请输入新版本项目描述，默认为（${comment}）`,
+  //       default: comment
+  //     }
+  //   ]);
+  //   console.log("🚀 ~ file: miniprogram-ci.js:12 ~ answer", answer);
+
+  // check in main branch
+  const curbranch = getGitBranch();
+  if (curbranch !== "main" && curbranch !== "master") {
+    console.log("\x1b[41m%s\x1b[0m", "请在主分支发布");
+    return;
+  }
+
+  // check the code enters the repository
   const res = execa.execaCommandSync(`git status`);
   if (!res.stdout.includes("nothing to commit")) {
-    console.log("\x1b[43m%s\x1b[0m", "请将代码提交至本地仓库");
+    const lastComment = getGitLastMsg("%s");
+    const a = execa.execaCommandSync(`git add .`);
+    const res2 = execa.execaSync("git", ["commit", "-m", lastComment]);
+    console.log("🚀 ~ file: miniprogram-ci.js:23 ~ res", res2, lastComment);
+    console.log("\x1b[43m%s\x1b[0m", "代码提交至本地仓库");
     return;
   }
 
@@ -15,15 +40,9 @@
     return res.stdout;
   }
 
-  function getGitLastMsg() {
-    const res = execa.execaCommandSync(
-      "git log --pretty=format:“%an/%cd/%s” -1"
-    );
+  function getGitLastMsg(format) {
+    const res = execa.execaCommandSync(`git log --pretty=format:${format} -1`);
     return res.stdout;
-  }
-  const curbranch = getGitBranch();
-  if (curbranch == "main" || curbranch !== "master") {
-    console.log("\x1b[41m%s\x1b[0m", "请在主分支发布");
   }
 
   console.log("process.argv", process.argv.slice(2)[0]);
@@ -39,14 +58,16 @@
   const uploadResult = await ci.upload({
     project,
     version: version.getVersion(),
-    desc: process.argv.slice(2)[0] || getGitLastMsg(),
+    desc: comment,
     setting: {
       minifyJS: true,
       minifyWXML: true,
       minifyWXSS: true,
       minify: true
     },
-    onProgressUpdate: console.log
+    onProgressUpdate: () => {
+      console.log("building...");
+    }
   });
   if (uploadResult) {
     version.updateVersion();
